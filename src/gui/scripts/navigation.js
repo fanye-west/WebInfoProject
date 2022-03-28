e2f19fcfe5a200776f9240712b80757d249e08f1
Grant Holtes <gwholtes@gmail.com>
2022-03-28 22:36:24 +1100

    Added menu

---
+++ src/gui/scripts/navigation.js
@@ -0,0 +1,10 @@
+//Functions for site navigation 
+
+function hamburgerExpander() {
+    console.log("EXPANDING");
+    document.getElementById("navigation_hamburger_clicked_modal").style.display = "inline";
+}
+
+function hamburgerHider() {
+    document.getElementById("navigation_hamburger_clicked_modal").style.display = "none";
+}
