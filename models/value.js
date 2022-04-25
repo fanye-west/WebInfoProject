a7d3d26496916c76f0ddf5a3cd1ee0bec7a6103d
aditya-ajit <76168207+aditya-ajit@users.noreply.github.com>
2022-04-25 22:11:35 +1000

    updated valueschema

--- models/value.js
+++ models/value.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const valueSchema = new mongoose.Schema({
     is_recorded: Boolean,
-    is_required: Boolean,
     value: Number,
     comment: String,
     when: { type: Date, default: Date.now }
