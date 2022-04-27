dc58d9d3e84dc9918f70c84d7f7b72ea77cd9557
Grant Holtes <gwholtes@gmail.com>
2022-04-27 22:46:00 +1000

    Added handlebars and past data to record

---
+++ config/utils.js
@@ -0,0 +1,6 @@
+const isToday = (someDate) => {
+    const today = new Date()
+    return someDate.getDate() == today.getDate() &&
+        someDate.getMonth() == today.getMonth() &&
+        someDate.getFullYear() == today.getFullYear()
+}
--- controllers/patientController.js
+++ controllers/patientController.js
@@ -8,9 +8,23 @@ require('../models')
 
 const mock_patient = require('../models/mock_patient.js')
 
+var green = "background-color:#9AD3A5";
+var red = "background-color:#E58783";
+var orange = "background-color:#F2CA95";
+
 //Deliverable 2 Hardcoded values
 const PatientID = "62668042f2c4e1d37f21d7b2"
 
+// utils
+function isToday(date) {
+    const today = new Date()
+    let check = date.getDate() == today.getDate() &&
+        date.getMonth() == today.getMonth() &&
+        date.getFullYear() == today.getFullYear()
+    return check
+}
+
+// Main functions
 const getPatientDash = async(req, res, next) => {
     try {
         // console.log(req.params.userId);
@@ -28,10 +42,111 @@ const getPatientDataEntry = async(req, res, next) => {
         // Get patient data for today's date
         const patientData = await Patient.findById(PatientID).lean()
         let latest_data = patientData.data[patientData.data.length - 1];
-        // if latest_data.date
-        console.log(latest_data);
-        testdata = { testvalue: 1234 }
-        return res.render('patientDataEntry', { layout: 'patientLayout', patient: testdata });
+
+        // var d = new Date();
+        // d.setDate(d.getDate() - 1);
+        // latest_data.date = d
+        // console.log(latest_data.date)
+
+        let glucose_value = undefined
+        let weight_value = undefined
+        let insulin_value = undefined
+        let exercise_value = undefined
+
+        let glucose_comment = undefined
+        let weight_comment = undefined
+        let insulin_comment = undefined
+        let exercise_comment = undefined
+
+        if (isToday(latest_data.date)) {
+            glucose_value = latest_data.glucose.value
+            weight_value = latest_data.weight.value
+            insulin_value = latest_data.insulin.value
+            exercise_value = latest_data.exercise.value
+
+            glucose_comment = latest_data.glucose.comment
+            weight_comment = latest_data.weight.comment
+            insulin_comment = latest_data.insulin.comment
+            exercise_comment = latest_data.exercise.comment
+        }
+
+        let glucose_colour = green;
+        let weight_colour = green;
+        let insulin_colour = green;
+        let exercise_colour = green;
+
+        let glucose_subheading = "";
+        let weight_subheading = "";
+        let insulin_subheading = "";
+        let exercise_subheading = "";
+
+        if (glucose_value == undefined && patientData.glucose_required) {
+            glucose_colour = red;
+            glucose_subheading = "You still need to fill this in for today"
+        } else if (glucose_value == undefined && !patientData.glucose_required) {
+            glucose_colour = orange;
+            glucose_subheading = "Optional record"
+        } else {
+            glucose_colour = green;
+            glucose_subheading = "You have already filled this in for today";
+        }
+
+        if (weight_value == undefined && patientData.weight_required) {
+            weight_colour = red;
+            weight_subheading = "You still need to fill this in for today"
+        } else if (weight_value == undefined && !patientData.weight_required) {
+            weight_colour = orange;
+            weight_subheading = "Optional record"
+        } else {
+            weight_colour = green;
+            weight_subheading = "You have already filled this in for today";
+        }
+
+        if (insulin_value == undefined && patientData.insulin_required) {
+            insulin_colour = red;
+            insulin_subheading = "You still need to fill this in for today"
+        } else if (insulin_value == undefined && !patientData.insulin_required) {
+            insulin_colour = orange;
+            insulin_subheading = "Optional record"
+        } else {
+            insulin_colour = green;
+            insulin_subheading = "You have already filled this in for today";
+        }
+
+        if (exercise_value == undefined && patientData.exercise_required) {
+            exercise_colour = red;
+            exercise_subheading = "You still need to fill this in for today"
+        } else if (exercise_value == undefined && !patientData.exercise_required) {
+            exercise_colour = orange;
+            exercise_subheading = "Optional record"
+        } else {
+            exercise_colour = green;
+            exercise_subheading = "You have already filled this in for today";
+        }
+
+
+
+        // glucose_subheading
+
+        patentExistingRecordData = {
+            glucose_value: glucose_value,
+            weight_value: weight_value,
+            insulin_value: insulin_value,
+            exercise_value: exercise_value,
+            glucose_comment: glucose_comment,
+            weight_comment: weight_comment,
+            insulin_comment: insulin_comment,
+            exercise_comment: exercise_comment,
+            glucose_colour: glucose_colour,
+            weight_colour: weight_colour,
+            insulin_colour: insulin_colour,
+            exercise_colour: exercise_colour,
+            glucose_subheading: glucose_subheading,
+            weight_subheading: weight_subheading,
+            insulin_subheading: insulin_subheading,
+            exercise_subheading: exercise_subheading
+        }
+        return res.render('patientDataEntry', { layout: 'patientLayout', patient: patentExistingRecordData });
         // return res.render('patientDash', { layout: 'patientLayout', patient: patientData });
     } catch (err) {
         return next(err)
@@ -53,8 +168,6 @@ const insertPatientData = async(req, res, next) => {
     // 		steps_comment: Free text comment for steps
     //Test with $ curl -X POST http://127.0.0.1:3000/user/patient/addpatientdata
     try {
-        //newAuthor = new Author(req.body)
-        //await newAuthor.save()
         const requestData = req.body;
         //Check data 
         //Numerical
